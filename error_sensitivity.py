more careful truncation solving bug when non hit was not properly shifted (so err sens had many outliers) refactoring of area computation
in meg beh processing -- working on diode handling

error_sensitivity.py
@@ -307,7 +307,7 @@ def computeErrSens3(behav_df, df_inds=None, epochs=None,
         raise ValueError(f'error_type={error_type} not implemnted')
 
     if recalc_non_hit or (colname_nh not in behav_df.columns):
-        raise ValueError('recalc_non_hit not implemneted')
+        raise ValueError(f'{colname_nh} is not present and recalc_non_hit not implemneted')
 
     nonhit = behav_df[colname_nh].to_numpy()
 
@@ -412,7 +412,7 @@ def computeErrSens3(behav_df, df_inds=None, epochs=None,
     df_esv = pd.DataFrame( df_esv )
 
 
-    df_esv[colname_nh] = nonhit
+    df_esv[colname_nh] = nonhit.astype(bool) 
     if colname_nh_out is None:
         colname_nh_out = colname_nh + '_shifted'
 
@@ -430,16 +430,18 @@ def computeErrSens3(behav_df, df_inds=None, epochs=None,
 
 
     #df_esv[colname_nh_out] = nonhit_shifted
-    df_esv[colname_nh_out] = nonhit_err_compat
+    df_esv[colname_nh_out] = nonhit_err_compat.astype(bool)
 
     df_esv[err_sens_coln] = np.nan
-    c = df_esv[colname_nh]
+    c = df_esv[colname_nh_out]
 
     if time_locked == 'trial_end':
         errors_denom = errors2
     else:
         errors_denom = errors1
-    df_esv.loc[c,err_sens_coln]  = (correction / errors_denom)[c]
+
+    #errors_denom[c]
+    df_esv.loc[c,err_sens_coln]  = (correction[c] / errors_denom[c])
 
     # recal that in the experiment code what goes to "errors" columns is
     # computed this way
@@ -449,30 +451,31 @@ def computeErrSens3(behav_df, df_inds=None, epochs=None,
     #import pdb; pdb.set_trace()
 
 
-    nh = np.sum( ~df_esv[colname_nh] )
+    nh = np.sum( ~df_esv[colname_nh_out] )
     if correct_hit == 'prev_valid':
-        df_esv.loc[~df_esv[colname_nh],err_sens_coln]  = np.inf
-        hit_inds = np.where(~df_esv[colname_nh] )[0]
+        df_esv.loc[~df_esv[colname_nh_out],err_sens_coln]  = np.inf
+        hit_inds = np.where(~df_esv[colname_nh_out] )[0]
         for hiti in hit_inds:
             prev = df_esv.loc[ :hiti, err_sens_coln ]
             good = np.where( ~ (np.isinf( prev ) | np.isnan(np.isinf) ) )[0]
             if len(good):
                 lastgood = good[-1]
                 df_esv.loc[ hiti, err_sens_coln ] = df_esv.loc[ lastgood, err_sens_coln ]
-        #df_esv.loc[~df_esv[colname_nh],err_sens_coln]  =
+        #df_esv.loc[~df_esv[colname_nh_out],err_sens_coln]  =
     elif correct_hit == 'zero':
         if verbose:
             print(f'correct_hit == {correct_hit}: setting {nh} out of {len(df_esv)}')
-        df_esv.loc[~df_esv[colname_nh],err_sens_coln]  = 0
+        df_esv.loc[~df_esv[colname_nh_out],err_sens_coln]  = 0
     elif correct_hit == 'inf':
         if verbose:
             print(f'correct_hit == {correct_hit}: setting {nh} out of {len(df_esv)}')
-        df_esv.loc[~df_esv[colname_nh],err_sens_coln]  = np.inf
+        df_esv.loc[~df_esv[colname_nh_out],err_sens_coln]  = np.inf
     elif correct_hit == 'nan':
         if verbose:
             print(f'correct_hit == {correct_hit}: setting {nh} out of {len(df_esv)}')
-        df_esv.loc[~df_esv[colname_nh],err_sens_coln]  = np.nan
+        df_esv.loc[~df_esv[colname_nh_out],err_sens_coln]  = np.nan
 
+    #raise ValueError('debug')
 
     df_esv['correction'] = correction
     df_esv['error_type'] = error_type

meg_proc.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-def events2df(events, info, dat_time, dat_diode, trigger2phase, CONTEXT_TRIGGER_DICT, restmintime):
+def events2df(events, raw, dat_time, dat_diode, trigger2phase, CONTEXT_TRIGGER_DICT, 
+              params, restmintime):
+    assert isinstance(params, dict)
     dfev = pd.DataFrame(events, columns=['nsample', 'prev_trigger', 'trigger' ]  )
     CONTEXT_TRIGGER_DICT_inv = dict(zip(CONTEXT_TRIGGER_DICT.values(), CONTEXT_TRIGGER_DICT.keys()))
     def f(row): 
@@ -23,7 +25,7 @@ def events2df(events, info, dat_time, dat_diode, trigger2phase, CONTEXT_TRIGGER_
     # this is 'naive time', when MEG is restarted (i.e. when multiple raws are concatenated)
     # it won't show any big difference between consequtive time values
     # in particular it will be inconsistent with the time values from the log
-    dfev['time'] = dfev['nsample'] / info['sfreq'] 
+    dfev['time'] = dfev['nsample'] / raw.info['sfreq'] 
 
     #### define better times
     dfev['time_megchan'] = dat_time[0,dfev['nsample']]
@@ -80,22 +82,26 @@ def events2df(events, info, dat_time, dat_diode, trigger2phase, CONTEXT_TRIGGER_
 
     #######################
 
+    #mvt_starts = df.query('phase == "TARGET_AND_FEEDBACK"').groupby('trial_index').min('time')
+    mvt_starts_megtrig = dfev.query('phase == "TARGET_AND_FEEDBACK"')
+
     # take first time we start movement from the point of view of dfev, 
     # take the value of the diode right before and in the middle of the mvt
     safe_delay = 200
     # we really need to use 'time' (which is raw.times basically) for time_as_index to work
     inds = raw.time_as_index(mvt_starts_megtrig['time'] - safe_delay)
-    diode_pos = diode[0, inds]
+    diode_pos = dat_diode[0, inds]
 
     safe_delay2 = params['time_feedback'] / 2
     inds2 = raw.time_as_index(mvt_starts_megtrig['time'] + safe_delay2)
-    diode_neg = diode[0, inds2]
+    diode_neg = dat_diode[0, inds2]
 
-    med = np.mean([ np.mean(diode_neg), np.mean(diode_pos)] )
-    print('med = ', med)
+    diode_extremes = [ np.mean(diode_neg), np.mean(diode_pos)]
+    med = np.mean(diode_extremes )
+    print('diode med = ', med, ' extremes ',diode_extremes)
 
 
-    dfdi = pd.DataFrame({'diode':dat_diode[0], 'times': times} )
+    dfdi = pd.DataFrame({'diode':dat_diode[0], 'times': raw.times} )
     #med = dfdi['diode'].median()
     #med = - 0.18 # MAY NEED TO BE TUNED MANUALLY! or I have to use info from dfev
     dfdi['diode_neg'] = dfdi['diode'] < med
@@ -112,20 +118,27 @@ def events2df(events, info, dat_time, dat_diode, trigger2phase, CONTEXT_TRIGGER_
 
     ###################
     # times when diode changed from off to on, first occurence
-    ts_di = grp.min('time_since_first_REST')['time_mmvt_starts_megtrigegchan'].values
+    ts_di = grp.min('time_since_first_REST')['time_since_first_REST'].values
+    #ts_di = grp.min('time_since_first_REST')['time_mvt_starts_megtrigchan'].values
 
     # times when diode changed from ON to OFF, first occurence
     ts_di_off = grp.max('time_since_first_REST') #+ 1/raw.info['sfreq']
     ts_di_off['time_since_first_REST'] += 1/raw.info['sfreq']
 
-    ts_ev = mvt_starts_megtrig['time_since_first_REST'].values
-    ts_diev_diff = ts_di - ts_ev
-    print('max,min,mean = ', np.max(ts_diev_diff), np.min(ts_diev_diff), np.mean(ts_diev_diff) )
 
-    #################
-    dfev['time_since_first_REST_diode'] = np.nan
-    dfev.loc[mvt_starts_megtrig.index, 'time_since_first_REST_diode'] = ts_di
-    dfev.loc[iti_starts_megtrig.index, 'time_since_first_REST_diode'] = ts_di_off
+    ts_ev = mvt_starts_megtrig['time_since_first_REST'].values
+
+    if len(ts_di) ==  len(ts_ev):
+        ts_diev_diff = ts_di - ts_ev
+        print('max,min,mean = ', np.max(ts_diev_diff), np.min(ts_diev_diff), np.mean(ts_diev_diff) )
+
+        #################
+        dfev['time_since_first_REST_diode'] = np.nan
+        dfev.loc[mvt_starts_megtrig.index, 'time_since_first_REST_diode'] = ts_di
+        dfev.loc[iti_starts_megtrig.index, 'time_since_first_REST_diode'] = ts_di_off
+    else:
+        print(f'WARNING: number of diod switches {(len(ts_di))} not equals number of triggers of start of movement {len(ts_ev)}')
+        print('Therefore cannot set time_since_first_REST_diode column')
 
 
     ########################